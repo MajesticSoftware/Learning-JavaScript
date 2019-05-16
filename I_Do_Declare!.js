d9a0e4b444629cd80b15ce54a3742db5365dd80c
MajesticSoftware <33467103+MajesticSoftware@users.noreply.github.com>
2019-05-16 18:10:00 -0400

    Add files via upload
    Declare variables of the following types:
    4 Numbers
    3 Strings
    2 Booleans
    1 Undefined Variable
    Give the variables descriptive names (ex: string1 and string2)
    Print each one to the console with a description (ex: console.log("The First String:", string1);)

---
+++ I_Do_Declare!.js
@@ -0,0 +1,24 @@
+var uno = 97
+var dos = 78
+var tres = 1.567
+var cuatro= -65874
+string1 = ("I know a bit of spanish")
+string2 = ("I'm bilingual in Python")
+string3 = ("I'm currently learning Javascript")
+var codingislyfe = true
+var codingisnotlyfe = false
+var empty = ""
+
+console.log("Variable one is " + uno);
+console.log("Variable two is " + dos);
+console.log("Variable three is " + tres);
+console.log("Variable four is " + cuatro);
+console.log("The first string:", string1)
+console.log("The second string:", string2)
+console.log("The third string:", string3)
+console.log("Is coding life?:", codingislyfe)
+console.log("Is coding not life?", codingisnotlyfe)
+
+
+
+
