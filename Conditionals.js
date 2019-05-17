55742eae74d740cce410f67eacb04dabfb8275d1
MajesticSoftware <33467103+MajesticSoftware@users.noreply.github.com>
2019-05-16 21:19:21 -0400

    Add files via upload
    Understanding Conditiontionals

---
+++ Conditionals.js
@@ -0,0 +1,14 @@
+myName = 'Jeff'
+
+if (myName== "Martin")
+{
+    console.log("Hey there Martin, how's it going?");
+}
+else if (myName == "Beth")
+{
+    console.log("You look fabulous today!");
+}
+else
+{
+    console.log("Greeting Eathling. Have a great day!");
+}
